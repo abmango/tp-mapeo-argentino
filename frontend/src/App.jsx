76e2765c0a6ac621ca38cb36f6a4d47e340afd60
EzequielFacu <facunmartinez@frba.utn.edu.ar>
2025-11-28 13:30:42 -0300

    creacion de nuevas rutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home/Home";
+import PageDesing from "./MainPageDesing/PageDesing";
+import AboutUsPage  from "./AboutUs/AboutUsPage";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route 
+          path='/' 
+          element={<Home/>}
+        >
+          <Route index element={<PageDesing/>}/>
+          <Route path="/nosotros" element={<AboutUsPage/>}/>
+          
+        </Route>
       </Routes>
     </BrowserRouter>
   )
