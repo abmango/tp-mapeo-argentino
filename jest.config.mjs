18c4c66bff0d7898d7643a658a2247589c1ab55f
Alexis Brian Mango <alexi@frba.utn.edu.ar>
2026-01-06 01:37:38 -0300

    Init Laravel backend

--- jest.config.mjs
+++
@@ -1,6 +0,0 @@
-export default {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest'
-  }
-};
--- src/Domain/Errors/DomainError.js
+++
@@ -1,9 +0,0 @@
-// src/domain/errors/DomainError.js
-class DomainError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "DomainError";
-  }
-}
-
-export default DomainError;
